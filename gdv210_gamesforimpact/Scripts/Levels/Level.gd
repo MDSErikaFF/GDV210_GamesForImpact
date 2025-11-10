64da64f7c9b2c820065400de6d73f642b9016edf
MDSErikaFF <Erika.Fernandes@mds.ac.nz>
2025-11-11 12:44:56 +1300

    Some small changes in folder structure

--- gdv210_gamesforimpact/Scripts/Levels/Level.gd
+++ gdv210_gamesforimpact/Scripts/Levels/Level.gd
@@ -5,10 +5,10 @@ var ActivatePollution : bool = false;
 @onready var CollisionLayer : TileMapLayer = $"Level-Collision"
 @onready var NonCollisionLayer : TileMapLayer = $"Level-DecorationsLayer"
 
-@onready var Col : TileSet = preload("res://Scripts/level/CollisionOnTS.tres");
-@onready var NonCol : TileSet = preload("res://Scripts/level/CollisionOffTS.tres");
-@onready var PollutionNonCol : TileSet = preload("res://Scripts/level/CollisionOffTSPollution.tres");
-@onready var PollutionCol : TileSet = preload("res://Scripts/level/CollisionOnTSPollution.tres");
+@onready var Col : TileSet = preload("res://Scripts/Levels/Other/CollisionOnTS.tres");
+@onready var NonCol : TileSet = preload("res://Scripts/Levels/Other/CollisionOffTS.tres");
+@onready var PollutionNonCol : TileSet = preload("res://Scripts/Levels/Other/CollisionOffTSPollution.tres");
+@onready var PollutionCol : TileSet = preload("res://Scripts/Levels/Other/CollisionOnTSPollution.tres");
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -20,9 +20,9 @@ func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("ActivatePollution"):
 		ActivatePollution != ActivatePollution;
 		if (ActivatePollution):
-			var Pollution = load("res://Scripts/level/CollisionOnTSPollution.tres");
+			var Pollution = load("res://Scripts/Levels/Other/CollisionOnTSPollution.tres");
 			CollisionLayer.tile_set = Pollution;
-			Pollution = load("res://Scripts/level/CollisionOffTSPollution.tres");
+			Pollution = load("res://Scripts/Levels/Other/CollisionOffTSPollution.tres");
 			NonCollisionLayer.tile_set = Pollution;
 			CollisionLayer.force_update_transform();
 			NonCollisionLayer.force_update_transform();
